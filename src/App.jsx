fbdbfe3d830aea6d7691942be492cd955a68f959
Vidal <97807522+RPVidal@users.noreply.github.com>
2025-10-20 00:23:49 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({titleObject}) => {
+
+  const [hasLiked, setHasLiked] = useState(false)
+  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    if (count % 2 === 0) {
+      console.log("Card hasLiked", hasLiked)
+    }
+  }, [hasLiked])
+
+
+  useEffect(() => {
+    if (count > 10) {
+      setCount(0)
+    }
+  }, [count])
+
+  return (
+    <div onClick={() => setCount(count + 1)}>
+      <h2>{titleObject.movieTitle}</h2>
+      <button onClick={() => setHasLiked(!hasLiked) }>{hasLiked ? `${count} 🤍` : `${count} ❤️`}</button>
+    </div>
+  )
+}
+
+function App() {
+  
+
+  return ( 
+    <>
+      <h1>Rv app</h1>
+        <Card titleObject={{movieTitle : "Card 1"}}/>
+        <Card titleObject={{movieTitle : "Card 2"}}/>
+    </>
+  )
+}
+
+export default App
